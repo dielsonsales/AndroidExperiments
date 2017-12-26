0d84b52767ca3776851debded8de0fbff77c5597
Dielson Sales <dielsonscarvalho@gmail.com>
2017-12-26 18:29:10 -0300

    Add ClickableSpan sample

--- app/src/main/java/io/github/dielsonsales/experiments/spanned/SpannedActivity.kt
+++ app/src/main/java/io/github/dielsonsales/experiments/spanned/SpannedActivity.kt
@@ -1,7 +1,15 @@
 package io.github.dielsonsales.experiments.spanned
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Toast
 import io.github.dielsonsales.experiments.R
 import kotlinx.android.synthetic.main.activity_spanned.*
 
@@ -14,7 +22,24 @@ class SpannedActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spanned)
 
-        spanned_text.text = "Okay, hehe"
+        val clickableSpan = object: ClickableSpan() {
+            override fun onClick(widget: View?) {
+                Toast.makeText(this@SpannedActivity, "Clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint?) {
+                super.updateDrawState(ds)
+                ds?.color = Color.RED
+                ds?.isUnderlineText = false
+            }
+        }
+
+        val spannedText = SpannableString("Hello, @dielsonsales")
+        spannedText.setSpan(clickableSpan, 7, 20, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        spanned_text.highlightColor = Color.TRANSPARENT
+        spanned_text.movementMethod = LinkMovementMethod.getInstance()
+        spanned_text.text = spannedText
     }
 
 }
